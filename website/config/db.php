95da6707d4c7036b2f61210660e66b3ea298e45f
elzadj <sukhoveyko@gmail.com>
2015-12-04 19:14:18 +0200

    Version 3.1.2

---
+++ website/config/db.php
@@ -0,0 +1,28 @@
+<?php
+///*
+define('DB_CMS_HOST', 'elephant.dns-systems.net');
+define('DB_CMS_PORT', 3306);
+define('DB_CMS_USER', 'elephant_cmsddes');
+define('DB_CMS_PASS', 'ddesMdP65');
+define('DB_CMS_NAME', 'elephant_cmsddes');
+
+define('DB_SC_HOST', 'elephant.dns-systems.net');
+define('DB_SC_PORT', 3306);
+define('DB_SC_USER', 'elephant_sc_ddes');
+define('DB_SC_PASS', 'ddesMdP65');
+define('DB_SC_NAME', 'elephant_sc_ddes');
+//*/
+
+/*
+define('DB_CMS_HOST', 'localhost');
+define('DB_CMS_PORT', 3306);
+define('DB_CMS_USER', 'root');
+define('DB_CMS_PASS', '');
+define('DB_CMS_NAME', 'cms_ccg');
+
+define('DB_SC_HOST', 'localhost');
+define('DB_SC_PORT', 3306);
+define('DB_SC_USER', 'root');
+define('DB_SC_PASS', '');
+define('DB_SC_NAME', 'sc_ccg');
+//*/
---
+++ website/config/site.php
@@ -0,0 +1,15 @@
+<?php
+define('CUSTOMER_NAME',  'Durham Dales, Easington and Sedgefield CCG');
+define('CUSTOMER_LOGO', 'ddes-logo.jpg');
+
+define('EARLIEST_DATE', '2014-12-01');
+define('QUESTION_ID', 'q_fft');
+define('OVERVIEW_PAGE', TRUE);
+define('DISPLAY_MENU', TRUE);
+
+define('PHRASING_RESPONDANT', 'patient');
+define('PHRASING_UNIT', 'practice');
+define('PHRASING_GROUP', 'CCG');
+define('PHRASING_FFT_FRAME_GROUP', 'Patients at our practices were asked:');
+define('PHRASING_FFT_FRAME_UNIT', 'Our patients were asked:');
+define('PHRASING_FFT_QUESTION', 'How likely are you to recommend our GP practice to friends and family if they needed similar care or treatment?');
